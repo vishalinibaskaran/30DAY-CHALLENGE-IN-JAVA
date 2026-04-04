b073bbfae929449083f8cb246d8cd840a064eee8
Vishalini B <vishalinib0930@gmail.com>
2026-04-04 21:37:36 +0530

    DAY 4-PalindromeNumber
    Given an integer x, return true if x is a palindrome, and false otherwise.

---
+++ PalindromeNumber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == rev;
+    }
+}
