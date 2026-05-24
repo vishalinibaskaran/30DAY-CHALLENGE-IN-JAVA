46e7743caf836a6e3210599ae09e736d4173b80e
Vishalini B <vishalinib0930@gmail.com>
2026-05-24 20:46:58 +0530

    DAY-19 perfect number
    A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
    
    Given an integer n, return true if n is a perfect number, otherwise return false.

---
+++ PerfectNumber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        
+        if(num <= 1) return false; // 1 is not perfect
+        
+        int sum = 1; // 1 is always divisor
+        
+        for(int i = 2; i * i <= num; i++) {
+            if(num % i == 0) {
+                sum += i;
+                
+                if(i != num / i) {
+                    sum += num / i;
+                }
+            }
+        }
+        
+        return sum == num;
+    }
+}
